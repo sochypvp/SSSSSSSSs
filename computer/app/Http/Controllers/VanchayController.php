5bcac38ebb9b3ea751683f73d6a9a2f6427e7d3d
chayozo <chaydouble0@gmail.com>
2024-02-26 15:06:30 +0700

    Add VanchayController

---
+++ computer/app/Http/Controllers/VanchayController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VanchayController extends Controller
+{
+    //
+    public function Vanchay(){
+        
+    }
+}
