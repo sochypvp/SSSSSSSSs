43b198e63bbb72a76a2de33392bb4e82ec633172
sochypvp <150105855+sochypvp@users.noreply.github.com>
2024-02-26 23:55:25 +0700

    ss

--- computer/resources/views/home.blade.php
+++ computer/resources/views/home.blade.php
@@ -5,8 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite(['resources/sass/home.scss'])
 </head>
 <body>
-    <h1>Home</h1>
+    <div class="container">
+        <nav>
+            <ul>
+                <li><img src="https://t3.ftcdn.net/jpg/03/45/37/04/360_F_345370423_NbsSK03bO91ldGdsn39cRarK3taW60FK.jpg" alt=""></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Product</a></li>
+                <li><a href="">Customer</a></li>
+                <li><a href="">Ordering</a></li>
+                <li><a href="">Order History</a></li>
+                <li><a href="">Manage Users</a></li>
+            </ul>
+        </nav>
+    </div>
 </body>
 </html>
---
+++ computer/resources/views/login.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/sass/app.scss'])
+</head>
+<body>
+    <form class="form-data" method="POST" enctype="multipart/form-data">
+        @csrf
+        <label for="">Login</label>
+        <input type="text" name="email" placeholder="Email">
+        <input type="text" name="password" placeholder="Password">
+        <div class="btn">
+            <a id="btn-login">Login</a>
+            <a id="btn-register">Register</a>
+        </div>
+    </form>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#btn-login').click(() => {
+            var data = $('.form-data').serialize();
+            $.ajax({
+                type: "post",
+                url: "{{route('Login')}}",
+                data: data,
+                dataType: "json",
+                success: function (response) {
+                    if(response.success){
+                        window.location.href = "{{route('Home')}}";
+                    }
+                }
+            });
+        });
+    });
+</script>
+</html>
