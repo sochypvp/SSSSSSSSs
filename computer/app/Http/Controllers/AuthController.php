ebf6d732d8e953c4ea6154b1490ab83a6bfd7a0b
sochypvp <150105855+sochypvp@users.noreply.github.com>
2024-02-27 16:28:15 +0700

    ss

--- computer/app/Http/Controllers/AuthController.php
+++ computer/app/Http/Controllers/AuthController.php
@@ -53,4 +53,9 @@ class AuthController extends Controller
             ]);
         }
     }
+
+    public function getData(){
+        $user = User::all();
+        return response($user);
+    }
 }
