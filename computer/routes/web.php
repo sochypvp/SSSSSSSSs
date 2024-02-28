250f913f8c90fb6e06ae809838c8e4cee9f47cf4
sochypvp <150105855+sochypvp@users.noreply.github.com>
2024-02-28 14:07:33 +0700

    ss

--- computer/routes/web.php
+++ computer/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-})->name('Home');
-
-Route::get('/login', function () {
     return view('login');
+})->name('login');
+
+Route::get('/home', function () {
+    return view('home');
 });
