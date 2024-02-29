bfa0b724890b304b2611e01884e3f32669120f4e
chayozo <chaydouble0@gmail.com>
2024-02-29 13:16:33 +0700

    Add new Controller Sub-category( function CRUD ) and Api Route of sub-category

--- computer/routes/api.php
+++ computer/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\cusUserController;
 use App\Http\Controllers\MainCategoriesController;
+use App\Http\Controllers\SubCategoriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::post('insertCus',[cusUserController::class, 'userInsert']);
 
 // Route of Main Categories
-Route::post('/dataMainCate', [MainCategoriesController::class, 'ShowMainCate']);
-Route::post('/AddMainCate', [MainCategoriesController::class, 'InsertData']);
+Route::post('/ShowMainCate', [MainCategoriesController::class, 'ShowMainCate']);
+Route::post('/InsertMainCate', [MainCategoriesController::class, 'InsertData']);
 Route::post('/DeleteMainCate', [MainCategoriesController::class, 'DeleteData']);
 Route::post('/UpdateMainCate', [MainCategoriesController::class, 'UpdateData']);
+
+// Route of Sub Categories
+Route::post('/ShowSubCate', [SubCategoriesController::class, 'ShowMainCate']);
+Route::post('/InsertSubCate', [SubCategoriesController::class, 'InsertData']);
+Route::post('/DeleteSubCate', [SubCategoriesController::class, 'DeleteData']);
+Route::post('/UpdateSubCate', [SubCategoriesController::class, 'UpdateData']);
+
